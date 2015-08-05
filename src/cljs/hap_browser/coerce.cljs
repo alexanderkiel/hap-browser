a7561fc54685bd74087f6acd423bb72440a760f6
Alexander Kiel <alexanderkiel@gmx.net>
2015-08-05 15:45:20 +0200

    Add Coercing of Vectors

--- src/cljs/hap_browser/coerce.cljs
+++ src/cljs/hap_browser/coerce.cljs
@@ -5,7 +5,12 @@
   (when (and (not (record? schema)) (map? schema))
     (c/safe c/edn-read-string)))
 
+(defn vector-matcher [schema]
+  (when (vector? schema)
+    (c/safe c/edn-read-string)))
+
 (defn coercion-matcher [schema]
   (or (c/+string-coercions+ schema)
       (c/keyword-enum-matcher schema)
-      (map-matcher schema)))
+      (map-matcher schema)
+      (vector-matcher schema)))
